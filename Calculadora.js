aebbf2320aaf804880c1233ea9a9d6755340e5ce
ValentinMiranda <elapapugod5@gmail.com>
2024-04-25 21:36:44 -0300

    Se añadio los 3 archivos css,html y js de calculadora, first commit

---
+++ Calculadora.js
@@ -0,0 +1,7 @@
+const variableDisplay = document.getElementById("display");
+const btn = document.querySelectorAll(".btn");
+btn.forEach(botones => {
+    botones.addEventListener("click", function(){
+        alert("sss")
+    })
+}); 
